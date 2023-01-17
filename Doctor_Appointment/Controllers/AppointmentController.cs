f28ac65b707a749aba0ce422dc5129f9fc3ba0ca
mhegs <mhegs@LAPTOP-NORCU771>
2023-01-17 23:07:59 +0800

    Add project files.

---
+++ Doctor_Appointment/Controllers/AppointmentController.cs
@@ -0,0 +1,25 @@
+﻿using Doctor_Appointment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Doctor_Appointment.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        static List<AppointmentModel> appointments = new List<AppointmentModel>();
+        public IActionResult Index()
+        {
+            return View(appointments);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        } 
+        
+        public IActionResult Details(AppointmentModel appointment) 
+        {
+            appointments.Add(appointment);
+            return View("Details", appointment);
+        }
+    }
+}
---
+++ Doctor_Appointment/Models/AppointmentModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Doctor_Appointment.Models
+{
+    public class AppointmentModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 4)]
+        [DisplayName("Patient's Full Name")]
+        public string? PatientName { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayName("Appointment Date and Time")]
+        public DateTime AppointmentDate { get; set; }
+
+        [DataType(DataType.Currency)]
+        [DisplayName("Patient's Payment")]
+        public decimal Payment { get; set; }
+
+        [DisplayName("Doctor's Full Name")]
+        public string? DoctorName { get; set; }
+
+        [Range(1, 10)]
+        [DisplayName("Patient's Perceived Level of Pain (1 low to 10 High")]
+        public int PainLevel { get; set; }
+
+    }
+}
